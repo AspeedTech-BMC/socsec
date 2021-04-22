4a1a9fe65960818674ae57202eb0007ba7d2c49b
Johnny Huang <johnny_huang@aspeedtech.com>
2021-04-22 13:59:21 +0800

    socsec: move hexdump to global
    move hexdump function from socsec to global
    
    Signed-off-by: Johnny Huang <johnny_huang@aspeedtech.com>

--- socsec/__init__.py
+++ socsec/__init__.py
@@ -23,6 +23,7 @@ import os
 from pkg_resources import resource_filename as pkgdata
 import struct
 from Crypto.PublicKey import RSA
+import binascii
 
 
 def parse_path(path):
@@ -123,6 +124,58 @@ def rsa_key_to_bin(rsa_key_file, types, order='little'):
     insert_bytearray(exp, key_bin, m_len)
 
     return key_bin
+
+
+def chunks(seq, size):
+    '''Generator that cuts sequence (bytes, memoryview, etc.)
+       into chunks of given size. If `seq` length is not multiply
+       of `size`, the lengh of the last chunk returned will be
+       less than requested.
+
+       >>> list( chunks([1,2,3,4,5,6,7], 3) )
+       [[1, 2, 3], [4, 5, 6], [7]]
+    '''
+    d, m = divmod(len(seq), size)
+    for i in range(d):
+        yield seq[i*size:(i+1)*size]
+    if m:
+        yield seq[d*size:]
+
+
+def hexdump(data):
+    '''
+    Transform binary data to the hex dump text format:
+
+    00000000: 00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  ................
+    '''
+    generator = chunks(data, 16)
+    for addr, d in enumerate(generator):
+        # 00000000:
+        line = '%08X: ' % (addr*16)
+        # 00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
+        dumpstr = ' '.join(
+            chunks(binascii.hexlify(d).decode('ascii').upper(), 2))
+        line += dumpstr[:8*3]
+        if len(d) > 8:  # insert separator if needed
+            line += ' ' + dumpstr[8*3:]
+        # ................
+        # calculate indentation, which may be different for the last line
+        pad = 2
+        if len(d) < 16:
+            pad += 3*(16 - len(d))
+        if len(d) <= 8:
+            pad += 1
+        line += ' '*pad
+
+        for byte in d:
+            # printable ASCII range 0x20 to 0x7E
+            if 0x20 <= byte <= 0x7E:
+                line += chr(byte)
+            else:
+                line += '.'
+        print(line)
+
+
 
 
 class OTP_info(object):
